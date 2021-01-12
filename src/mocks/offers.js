921b24d859fba44d3e86d8fa2ba21088e2ca6a34
barinovalex <67120204+barinovalex@users.noreply.github.com>
2021-01-12 18:22:45 +0300

    add offers mock

---
+++ src/mocks/offers.js
@@ -0,0 +1,43 @@
+export const offers = [
+  {
+    name: `Beautiful &amp; luxurious apartment at great location`,
+    type: `Apartment`,
+    rating: 0.8,
+    price: 120,
+    priceText: `night`,
+    image: `img/apartment-01.jpg`,
+    mark: `Premium`,
+    inBookmarks: false,
+  },
+  {
+    name: `Wood and stone place`,
+    type: `Private room`,
+    rating: 0.8,
+    price: 80,
+    priceText: `night`,
+    image: `img/room.jpg`,
+    mark: `none`,
+    inBookmarks: true,
+  },
+  {
+    name: `Canal View Prinsengracht`,
+    type: `Apartment`,
+    rating: 0.8,
+    price: 132,
+    priceText: `night`,
+    image: `img/apartment-02.jpg`,
+    mark: `none`,
+    inBookmarks: false,
+  },
+  {
+    name: `Nice, cozy, warm big bed apartment`,
+    type: `Apartment`,
+    rating: 1,
+    price: 180,
+    priceText: `night`,
+    image: `img/apartment-03.jpg`,
+    mark: `Premium`,
+    inBookmarks: false,
+  }
+];
+
