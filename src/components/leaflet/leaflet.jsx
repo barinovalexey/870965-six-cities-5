9479c201e462e2451c60612223330af11706cc04
barinovalex <67120204+barinovalex@users.noreply.github.com>
2021-01-20 14:35:25 +0300

    add leaflet snapshot test some refactoring

--- src/components/leaflet/leaflet.jsx
+++ src/components/leaflet/leaflet.jsx
@@ -5,48 +5,49 @@ import L from 'leaflet';
 export default class Leaflet extends PureComponent {
   constructor(props) {
     super(props);
+    this.mapRef = React.createRef();
   }
 
   componentDidMount() {
     const {offers, city, zoom} = this.props;
-    this.map = L.map(`map`, {
-      center: city,
-      zoom,
-      zoomControl: false,
-      marker: true
-    });
+    if (this.mapRef && this.mapRef.current) {
+      this.map = L.map(this.mapRef.current, {
+        center: city,
+        zoom,
+        zoomControl: false,
+        marker: true
+      });
 
-    const icon = L.icon({
-      iconUrl: `img/pin.svg`,
-      iconSize: [30, 30]
-    });
+      const icon = L.icon({
+        iconUrl: `img/pin.svg`,
+        iconSize: [30, 30]
+      });
 
 
-    L.tileLayer(`https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png`, {
-      attribution: `&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>`
-    })
-      .addTo(this.map);
-
-    offers.forEach((item) => {
-      const offerCords = item.coords;
-      L
-        .marker(offerCords, {icon})
+      L.tileLayer(`https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png`, {
+        attribution: `&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>`
+      })
         .addTo(this.map);
-    });
+
+      offers.forEach((item) => {
+        const offerCords = item.coords;
+        L
+          .marker(offerCords, {icon})
+          .addTo(this.map);
+      });
+    }
   }
 
   render() {
     return (
-      <div id="map" style={{height: `100%`}}></div>
+      <div ref={this.mapRef} style={{height: `100%`}}></div>
     );
   }
 }
 
 Leaflet.propTypes = {
-  offers: PropTypes.array({
-    coords: PropTypes.array.isRequired,
-  }),
+  offers: PropTypes.array.isRequired,
   city: PropTypes.array.isRequired,
-  zoom: PropTypes.isRequired
+  zoom: PropTypes.number.isRequired
 };
 
